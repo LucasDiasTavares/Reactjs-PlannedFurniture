a66bbcd67ccb3b8e69b287c10934e7d73ecb41fa
Tavares <lucas.tavares1996@hotmail.com>
2020-04-17 12:39:30 -0300

    Terminando a página de detalhes de cada movel

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import PlannedFurniture from './pages/PlannedFurniture';
-import SingleFurniture from './pages/SingleFurniture';
+import SingleFurniture from './pages/SingleFurniture/SingleFurniture';
 import Error from './pages/Error';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
---
+++ src/pages/SingleFurniture/SingleFurniture.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import defaultBcg from '../../images/defaultBcg.jpg'
+import Banner from '../../components/Banner/Banner'
+import { Link } from 'react-router-dom'
+import { FurnitureContext } from '../../Contex'
+import StyledHero from '../../components/Header/StyledHero'
+import './style.css'
+
+
+
+export default class SingleFurniture extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        }
+    }
+
+    static contextType = FurnitureContext;
+
+    render() {
+        const { getFurniture } = this.context;
+        const furniture = getFurniture(this.state.slug);
+        if (!furniture) {
+            return <div className="error">
+                <h3>No such furtniture could be found</h3>
+                <Link to='/' className='btn-primary'>Back to home</Link>
+            </div>
+        }
+
+        const { name, description, capacity, size, price, extras, images, mirror } = furniture
+        const [mainImg, ...otherImgs] = images;
+        return (
+            <>
+                <StyledHero img={mainImg}>
+                    <Banner title={`${name} furniture`}>
+                        <Link className='btn-primary' to='/'>back to furnitures</Link>
+                    </Banner>
+                </StyledHero>
+                <section className='single-furniture'>
+                    <div className="single-furniture-images">
+                        {otherImgs.map((item, index) => {
+                            return <img key={index} src={item} alt={name} />
+                        })}
+                    </div>
+                    <div className="single-furniture-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price: ${price}</h6>
+                            <h6>Height: {size} </h6>
+                            <h6>Width: {size} </h6>
+                            <h6>
+                                max capacity: {
+                                    capacity > 1 ? `${capacity} people` : `${capacity} person`
+                                }
+                            </h6>
+                            <h6>
+                                {mirror ? "contains mirror" : "not contains mirror"}
+                            </h6>
+                        </article>
+                    </div>
+                </section>
+                <section className="furniture-extras">
+                    <h6>extras</h6>
+                    <ul className="extras">
+                        {extras.map((item, index) => {
+                            return <li key={index} > - {item}</li>
+                        })}
+                    </ul>
+                </section>
+            </>
+        )
+    }
+}
